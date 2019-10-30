b44fbdc110dc51b97b3301c07a022e2bda957756
JL <jelena.cmila@gmail.com>
2019-10-30 19:11:17 -0400

    analysis update

--- apps/cityPage.py
+++ apps/cityPage.py
@@ -1,7 +1,8 @@
 import dash_html_components as html
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
+import json
 from app import app
 import udf
 
@@ -11,7 +12,7 @@ import udf
 def returnScatter():
     return html.Div([html.H3('Roles Count & Salary by Company',
                              style={"textAlign": "center"}),
-                     html.P('Description', style={'padding': '10px 10px',
+                     html.P('Hover over any of the points to discover which city it represents. Click on the point to continue analysing the patterns for chosen city. Switch between displaying mean, maximum or minimum salary. ', style={'padding': '10px 10px',
                                                   "textAlign": "center"}),
          dcc.RadioItems(
              id='choice-items1',
@@ -21,8 +22,8 @@ def returnScatter():
              inputStyle={'display': 'inline-block',
                          'padding': '0 0'}),
         dcc.Graph(id='crossfilter-scatter1',
-                  figure=udf.create_scatter("mean"),
-                  hoverData={'points': [{'customdata': 'NEW YORK',
+                  figure=udf.create_scatter("mean", "NEW YORK"),
+                  clickData={'points': [{'customdata': 'NEW YORK',
                                          'text': 'NEW YORK'}]}
                   ),
                               ], style={'width': '50%',
@@ -33,32 +34,35 @@ def returnScatter():
 def returnTopCompanies():
     return html.Div([html.H3('Top Companies Analysis',
                              style={"textAlign": "center"}),
-                     html.P('Description', style={'padding': '10px 10px',
+                     html.P('Analyse the quantity of roles available as well as salary in the top 20 employers for a chosen city.', style={'padding': '10px 10px',
                                                   "textAlign": "center"}),
         dcc.Graph(id='top_companies_fig1',
-                     figure=udf.create_top_comp_analysis("NEW YORK", "mean"))
+                     figure=udf.createMultiGraph(udf.getDataTopComp("NEW YORK", "mean"), "None"))
                      ], style={'width': '70%',
-                               'height': '700px', 'margin': '20px 250px'})
+                               'height': '800px', 'margin': '20px 250px'})
 
 
 def returnTopRoles():
     return html.Div([html.H3('Top Roles Analysis',
                              style={"textAlign": "center"}),
-                     html.P('Description', style={'padding': '10px 10px',
+                     html.P("Analyse the quantity of various roles available as well as salary for a chosen city. Roles are displayed as a main \'root\' word extracted from the original job title to present similar roles as one. Our analysis show that around 82% of job titles have at least one of these \'root\' words in it", style={'padding': '10px 10px',
                                                   "textAlign": "center"}),
-        dcc.Graph(id='top_roles_fig1')
-                     ], style={'width': '50%',
-                               'height': '600px', 'margin': '20px 450px'})
+        dcc.Graph(id='top_roles_fig1',
+                  clickData={'points': [{'y': 'analyst',
+                                         'text': 'analyst'}]},
+                  )
+                     ], style={'width': '70%',
+                               'height': '800px', 'margin': '20px 250px'})
 
 
 def returnSalDist():
     return html.Div([html.H3('Salary Distribution & Role Analysis',
                              style={"textAlign": "center"}),
-                     html.P('Description', style={'padding': '10px 10px',
+                     html.P("Analyst salaries vary widely. Discover which type of analysts have the highest pay. Check it out for other job roles by clicking on the corresponding bar plot", style={'padding': '10px 10px',
                                                   "textAlign": "center"}),
                     dcc.Graph(id='sal_dist_fig1')
-                     ], style={'width': '50%',
-                               'height': '600px', 'margin': '20px 450px'})
+                     ], style={'width': '70%',
+                               'height': '700px', 'margin': '20px 250px'})
 
 
 
@@ -66,6 +70,7 @@ def returnSalDist():
 layout = html.Div([
             html.Div([html.H1('Salary by Cities Analytics',
                               style={"textAlign": "center", 'padding': '20px 0 0 0'})]),
+                    html.Div(id='intermediate-value', style={'display': 'none'}),
                     html.Br(),
                     returnScatter(),
                     html.Br(),
@@ -81,10 +86,52 @@ layout = html.Div([
 
 """Tab1 Radio button"""
 @app.callback(Output('crossfilter-scatter1', 'figure'),
-              [Input('choice-items1', 'value')])
-def render_content(value):
-    return udf.create_scatter(str(value))
+              [Input('choice-items1', 'value'),
+               Input('crossfilter-scatter1', 'clickData')])
+def updateScatter(value, clickData):
+    city = clickData["points"][0]['customdata']
+    return udf.create_scatter(str(value), city)
 
+
+# @app.callback(Output('intermediate-value', 'children'),
+#               [Input('crossfilter-scatter1', 'clickData')])
+# def clean_data(clickData):
+#      city = clickData["points"][0]['customdata']
+#      # some expensive clean data step
+#      dff = udf.df[udf.df.city == city]
+#
+#      # more generally, this line would be
+#      # json.dumps(cleaned_df)
+#      return json.dumps(dff)
+
+"""Top Companies"""
+@app.callback(Output('top_companies_fig1', 'figure'),
+              [Input('choice-items1', 'value'),
+               Input('crossfilter-scatter1', 'clickData')])
+def updateTopCompanies(value, clickData):
+    city = clickData["points"][0]['customdata']
+    return udf.createMultiGraph(udf.getDataTopComp(city, str(value)), "None")
+
+
+"""Top Roles"""
+@app.callback(Output('top_roles_fig1', 'figure'),
+              [Input('choice-items1', 'value'),
+               Input('crossfilter-scatter1', 'clickData'),
+               Input('top_roles_fig1', 'clickData')])
+def updateTopCompanies(value, clickData, roleClickData):
+    city = clickData["points"][0]['customdata']
+    role = roleClickData["points"][0]['y']
+    return udf.createMultiGraph(udf.dataForTopRoles(city, str(value)), str(role))
+
+
+@app.callback(
+    Output('sal_dist_fig1', 'figure'),
+    [Input('crossfilter-scatter1', 'clickData'),
+    Input('top_roles_fig1', 'clickData')])
+def update_figure(clickData, roleClickData):
+    city = clickData["points"][0]['customdata']
+    role = roleClickData["points"][0]['y']
+    return udf.createBoxPlot(str(city), str(role))
 
 
 
--- udf.py
+++ udf.py
@@ -4,6 +4,7 @@ import dash_html_components as html
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
 import numpy as np
+import json
 
 """ Data -> was scraped for 2019 but as it turns out when request was made for 
 the company that did not have any visa applications in 2019 - the code automatically 
@@ -21,7 +22,7 @@ df = pd.read_csv(r"data/h1b19_clean.csv")
 """ Data clean and transform """
 
 
-def create_scatter(salary_choice):
+def create_scatter(salary_choice, city):
     x = df.groupby("city").agg({"company": "count",
                                 "base_salary": salary_choice}).sort_values(by="company",
                                                     ascending=False)[:500]["company"].values
@@ -33,8 +34,14 @@ def create_scatter(salary_choice):
     name = df.groupby("city").agg({"company": "count",
                                      "base_salary": salary_choice}).sort_values(by="company",
                                                     ascending=False)[:500].index.tolist()
-    return {
-        'data': [go.Scatter(
+
+    x_click = df[df.city == city].shape[0]
+    y_click = df[df.city == city].agg({"base_salary": salary_choice})[0]
+    name_click = city
+
+    traces = []
+
+    traces.append(go.Scatter(
             x=x,
             y=y,
             text=[i for i in name],
@@ -44,12 +51,26 @@ def create_scatter(salary_choice):
                 'size': 15,
                 'opacity': 0.8,
                 'line': {'width': 0.5, 'color': 'black'},
-                'color': '#f35588',
-            }
-        )],
+                'color': '#f35588',}
+        ))
+
+    traces.append(go.Scatter(
+        x=[x_click],
+        y=[y_click],
+        text=[name_click],
+        customdata=[name_click],
+        mode='markers',
+        marker={
+            'size': 15,
+            'line': {'width': 0.5, 'color': 'black'},
+            'color': '#71a95a'}
+    ))
+
+    return {
+        'data': traces,
         'layout': go.Layout(
             xaxis={'automargin': True,
-                'title': "count"
+                'title': "count",
             },
             yaxis={'automargin': True,
                 'title': salary_choice + " salary"
@@ -57,48 +78,113 @@ def create_scatter(salary_choice):
             margin={'l': 60, 'b': 50, 't': 50, 'r': 20},
             height=420,
             hovermode='closest',
+            showlegend=False,
         )
     }
 
 
-def create_top_comp_analysis(city, salary_choice):
-    top_companies = df[df.city == city].groupby("company").count()[\
-                        "job_title"].sort_values(ascending=\
-                                                         False)[:20].index.tolist()
-    dff = df[(df.company.isin(top_companies)) & (df.city == city)].drop(["state", "city"], axis=1)
+def getDataTopComp(city, salary_choice):
+    top_companies = df[df.city == city].groupby("company").count()["job_title"].sort_values(ascending=False)[
+                    :20].index.tolist()
+    dff = df[df.company.isin(top_companies)].drop(["state", "city"], axis=1)
     dff_gp = dff.groupby("company").agg( \
         {"job_title": "count", "base_salary": salary_choice} \
         ).rename(columns={"job_title": "count"}).sort_values(by="count", ascending=False)
 
     dff_gp = dff_gp.sort_values(by="count")
-    x = dff_gp.index
+    x = dff_gp.index.values
     y_count = dff_gp["count"].values
     y_choice = dff_gp["base_salary"].values
+    return city, salary_choice, x, y_count, y_choice
+
+
+def createMultiGraph(items, y_click):
+    # top_companies = df[df.city ==city].groupby("company").count()["job_title"].sort_values(ascending=False)[:20].index.tolist()
+    # dff = df[df.company.isin(top_companies)].drop(["state", "city"], axis=1)
+    # dff_gp = dff.groupby("company").agg( \
+    #     {"job_title": "count", "base_salary": salary_choice} \
+    #     ).rename(columns={"job_title": "count"}).sort_values(by="count", ascending=False)
+    #
+    # dff_gp = dff_gp.sort_values(by="count")
+    # x = dff_gp.index.values
+    # y_count = dff_gp["count"].values
+    # y_choice = dff_gp["base_salary"].values
+    city, salary_choice, x, y_count, y_choice = items
     fig = make_subplots(rows=1, cols=2, specs=[[{}, {}]], shared_xaxes=True,
                         shared_yaxes=False, vertical_spacing=0.001)
 
-    fig.append_trace(go.Bar(
-        x=y_count,
-        y=x,
-        marker=dict(
-            color='#71a95a',
-            line=dict(
+    zipped = list(zip(y_count, x, y_choice))
+    y_count, x, y_choice = zip(*sorted(zipped))
+    y_count = list(y_count)
+    x = list(x)
+    y_choice = list(y_choice)
+    # y_count_click = y_count
+    # x_li_click = x
+    # y_click = y_count.pop(x.index(y_click))
+    # x_click = x.pop(x.index(y_click))
+
+    if y_click == "None":
+        fig.append_trace(go.Bar(
+            x=y_count,
+            y=x,
+            marker=dict(
                 color='#71a95a',
-                width=1),
-        ),
-        name='Roles Count',
-        orientation='h',
-        width=0.4,
-    ), 1, 1)
+                line=dict(
+                    color='#71a95a',
+                    width=1),
+            ),
+            name='Roles Count',
+            orientation='h',
+            width=0.4,
+        ), 1, 1)
+    else:
+        # Selected
+        y_cl = y_count.pop(x.index(y_click))
+        x_cl = x.pop(x.index(y_click))
+        fig.append_trace(go.Bar(
+            x=[y_cl],
+            y=[x_cl],
+            marker=dict(
+                color='#f35588',
+                line=dict(
+                    color='#f35588',
+                    width=1),
+            ),
+            name='Roles Count - Selected',
+            orientation='h',
+            width=0.4,
+        ), 1, 1)
+        # Not Selected
+        fig.append_trace(go.Bar(
+            x=y_count,
+            y=x,
+            marker=dict(
+                color='#71a95a',
+                line=dict(
+                    color='#71a95a',
+                    width=1),
+            ),
+            name='Roles Count',
+            orientation='h',
+            width=0.4,
+        ), 1, 1)
+
+
 
     fig.append_trace(go.Scatter(
-        x=y_choice, y=x,
+        x=list(y_choice), y=list(x),
         mode='lines+markers',
         line_color='#f35588',
-        name= salary_choice + ' salary',
+        name=salary_choice + ' salary',
     ), 1, 2)
 
     fig.update_layout(
+        title=dict(text=city, font=dict(
+            size=20,
+            family='Comic Sans MS',
+            color="#f35588"
+        )),
+        
         yaxis=dict(
             showgrid=False,
             showline=False,
@@ -160,7 +246,89 @@ def create_top_comp_analysis(city, salary_choice):
 
     # fig.update_layout(annotations=annotations)
     return fig.to_dict()
-# create_top_comp_analysis("NEW YORK", "mean")
+
+
+
+"""" Roles Analsys """
+"""Support Function"""
+
+
+def getTokenSummary(dff, key_title_tokens):
+    index_dict = {}
+    for ind, row in enumerate(dff["tokens"]):
+        try:
+            for k in key_title_tokens:
+                if k in row.split():
+                    if k not in index_dict.keys():
+                        index_dict[k] = []
+                    index_dict[k].append(ind)
+        except Exception:
+            pass
+
+    # structure - > key -> all -> [count, mean, min, max] , different title names -> keep top 10 roles and how many it represents
+    result_dict = {}
+    for k in index_dict.keys():
+        df_temp = dff.iloc[index_dict[k]]
+        result_dict[k] = {}
+        # total
+        my_list = [df_temp.shape[0], int(df_temp["base_salary"].mean()),
+                   int(df_temp["base_salary"].min()),
+                   int(df_temp["base_salary"].max())]
+        result_dict[k]["all"] = my_list
+
+        # by job_title type -> top 10
+        top_roles = df_temp.groupby("tokens").agg({"company": "count", }).sort_values(by="company", ascending=False)[
+                    :10].index.tolist()
+        top_roles_perc = round(df_temp.groupby("tokens").agg( \
+            {"company": "count"} \
+            ).sort_values(by="company", \
+                          ascending=False)[:10].company.sum() / df_temp.shape[0], 2)
+        top_roles.append(top_roles_perc)
+        result_dict[k]["top_roles"] = top_roles
+
+    return result_dict
+
+
+key_title_tokens = ["engin", "develop", "architect", "administr", \
+                        "account", "auditor", \
+                        "analyst", "manag", "consult", "associ",\
+                        "presid", "programm", "professor"]
+
+def dataForTopRoles(city, salary_choice):
+    dff = df[df.city == city]
+    tokens_summary = getTokenSummary(dff, key_title_tokens)
+    x = [i for i in tokens_summary.keys()]
+    y_count = [tokens_summary[i]["all"][0] for i in [i for i in tokens_summary.keys()]]
+    y_choice = [tokens_summary[i]["all"][1] if
+                salary_choice == "mean" else tokens_summary[i]["all"][2]
+                if salary_choice == "min" else tokens_summary[i]["all"][3]
+                for i in x]
+    return city, salary_choice, x, y_count, y_choice
+
+
+def createBoxPlot(city, role):
+    dff = df[df.city == city]
+    tokens_summary = getTokenSummary(dff, key_title_tokens)
+    x = dff[dff.tokens.isin(tokens_summary[role]["top_roles"][:-1])]["tokens"].unique()
+    traces = []
+    for r in x:
+        traces.append(go.Box(y=dff[dff.tokens == r]["base_salary"],
+                         name=r, marker={"size": 4, "color": "#f35588"}))
+
+    return {"data": traces,
+            "layout": go.Layout(autosize=True,
+                                margin={"l": 50, "b": 200, "r": 20, "t": 10},
+                                xaxis={"showticklabels": True},
+                                yaxis={"title": "Salary Distribution"},
+                                showlegend=False)}
+
+
+
+
+
+
+
+
 
 
 """" Footer is the same on all pages - Store code here """
