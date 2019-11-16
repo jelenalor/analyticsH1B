50502c305f0c496398ce91bcc9a43ecd15cd5168
JL <jelena.cmila@gmail.com>
2019-11-16 10:49:33 -0500

    image

--- index.py
+++ index.py
@@ -16,7 +16,7 @@ server = app.server
 
 app.layout = html.Div([
 html.Div([
-    html.Img(src=app.get_asset_url('image.png'))
+    html.Img(src='/assets/image.png')
 ], ),
     dcc.Tabs(id="tabs", value='tab-1', children=[
         dcc.Tab(label='Explore by City', value='tab-1'),
