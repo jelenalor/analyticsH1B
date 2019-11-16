e2f3c2132e211165f007208d4e0a750a337bb4af
JL <jelena.cmila@gmail.com>
2019-11-15 23:29:55 -0500

    image

--- app.py
+++ app.py
@@ -3,5 +3,15 @@ import flask
 
 
 server = flask.Flask(__name__)
-app = dash.Dash(__name__, server=server)
+app = dash.Dash(__name__, server=server, meta_tags=[
+    # A description of the app, used by e.g.
+    # search engines when displaying search results.
+    {
+        'name': 'H1B Analytics',
+        'content': 'Analysis of public H1B salary data by city, company and job titles'
+    },
+    {
+        'property': 'og:image',
+        'content': 'assets/image.png'
+    }])
 app.config.suppress_callback_exceptions = True
