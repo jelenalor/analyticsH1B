50266e914cd6944374e286e86a74d6d10a2ad102
JL <jelena.cmila@gmail.com>
2019-11-16 11:47:40 -0500

    image

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import flask
 
 server = flask.Flask(__name__)
 app = dash.Dash(__name__,
+                static_folder='static',
                 server=server,
                 meta_tags=[
     # A description of the app, used by e.g.
